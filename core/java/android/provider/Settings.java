0c726a11a248bb09829bcf3e459c9c4fae22ac03
Carlo Savignano <carlosavignano@aospa.co>
2016-09-17 04:17:40 -0400

    input: hardware buttons lights integration
    * Enable and improve partial AOSP disabled button brightness hooks.
    * Implement user-timeout/doze/dream/awake/asleep policy.
    * Addresses NOUGAT-14
    
    Change-Id: Ie1fc9d1ff98e1782bdeb3afd03b03b762f39e5fc
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values-ru/nuclear_strings.xml
    	packages/DefaultContainerService/res/values-as-rIN/strings.xml
    	services/core/java/com/android/server/lights/LightsService.java
    	services/core/java/com/android/server/power/PowerManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3917,6 +3917,24 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
+         * Value for button brightness.
+         * This is an integer value in a range between 0 and 255.
+         *      0 = off
+         *    255 = default
+         *    255 = max
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS = "button_brightness";
+
+        /**
+         * Whether user can enable/disable button brightness.
+         * <p>
+         * Type: int (0 for false, 1 for true)
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS_ENABLED = "button_brightness_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
