8c7ea182abf80e29ced303731a92718cf9ab227b
beanstown106 <nbenis106@gmail.com>
2016-11-29 08:05:22 +0530

    Clock Seonds Options [1/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3900,6 +3900,13 @@ public final class Settings {
          */
         public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
 
+         /**
+         * Whether to show seconds next to clock in status bar
+         * default: off
+         * @hide
+         */
+        public static final String STATUS_BAR_CLOCK_SECONDS = "status_bar_clock_seconds";
+        
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
