02deb28149d20e8db49ae9a6565d7b4c0a44d54f
Moludos <Moledo.alex@gmail.com>
2016-12-08 17:53:00 +0100

    Update BootReceiver.java

--- packages/SystemUI/src/com/android/systemui/BootReceiver.java
+++ packages/SystemUI/src/com/android/systemui/BootReceiver.java
@@ -33,12 +33,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         try {
-            // Start the load average overlay, if activated
             ContentResolver res = context.getContentResolver();
-            if (Settings.Global.getInt(res, Settings.Global.SHOW_PROCESSES, 0) != 0) {
-                Intent loadavg = new Intent(context, com.android.systemui.LoadAverageService.class);
-                context.startService(loadavg);
-            }
             // Start the cpu info overlay, if activated
             if (Settings.Global.getInt(res, Settings.Global.SHOW_CPU, 0) != 0) {
                 Intent cpuinfo = new Intent(context, com.android.systemui.CPUInfoService.class);
