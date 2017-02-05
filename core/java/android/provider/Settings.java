37391f894f2fb25a38d21c91855698cb507769d0
xyyx <xyyx@mail.ru>
2017-02-05 14:31:26 +0800

    Unlock keystore with fingerprint after reboot [1/2]
    Change-Id: Idd710c2abef007663c95453cd6ffd4a549008465

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3923,6 +3923,12 @@ public final class Settings {
         public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
